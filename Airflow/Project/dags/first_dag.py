ff584a11d300cc054111e485c5c4e84a5ea91cd4
rushank7 <rushank.2008@gmail.com>
2023-02-01 21:48:41 +0100

    Airflow working with first dag created

--- Airflow/Project/dags/first_dag.py
+++ Airflow/Project/dags/first_dag.py
@@ -1,7 +1,8 @@
 try: 
     from datetime import timedelta
+    from datetime import datetime
     from airflow import DAG
-    from airflow.operators.python_operator import PythonOperator    from datetime import datetime
+    from airflow.operators.python_operator import PythonOperator
     print('all modules are printed')
 except Exception as e:
     print('error {}'.format(e))
@@ -11,17 +12,18 @@ def first_function_execute():
     return 'hello world'
 
 with DAG(
-    dag_id='first_Dag',
+    dag_id='first_dag',
     schedule_interval='@daily',
     default_args={'owner':'airflow',
     'retries':1,
     'retry_delay':timedelta(minutes=5),
-    'start_Date':datetime(2023,1,1)},
+    'start_date':datetime(2023,1,1)},
     catchup=False
 ) as f:
     
     first_function_execute = PythonOperator(
-        task_id = 'first_function_execute',
-        python_callable = 'first_function_execute'
-    )
+        task_id = "first_function_execute",
+        python_callable = first_function_execute)
 
+## docker container ls - shows all running container
+##  docker image prune - deletes all unnnecessary images
