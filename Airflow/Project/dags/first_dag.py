b2579bfe5cc27e63778ca310da2fa3607e7c5b09
rushank7 <rushank.2008@gmail.com>
2023-02-01 22:18:56 +0100

    final commit airflow tutorial

--- Airflow/Project/dags/first_dag.py
+++ Airflow/Project/dags/first_dag.py
@@ -7,9 +7,15 @@ try:
 except Exception as e:
     print('error {}'.format(e))
 
-def first_function_execute():
-    print('hellow world')
-    return 'hello world'
+def first_function_execute(**context):
+    print('first_function_execute')
+    context['ti'].xcom_push(key='mykey',value="first_function_execute says hello") ## get the key value and push the data/function
+
+## 2nd function
+
+def second_function_execute(**context):
+    instance = context.get("ti").xcom_pull(key="mykey")  ## get the key value and pull the data/function
+    print("i am in second_fucntion_execute got value :{} from function 1".format(instance))
 
 with DAG(
     dag_id='first_dag',
@@ -23,7 +29,16 @@ with DAG(
     
     first_function_execute = PythonOperator(
         task_id = "first_function_execute",
-        python_callable = first_function_execute)
+        python_callable = first_function_execute,
+        provide_context = True,
+        op_kwargs={"name":"rushankpatil"})
+
+    second_function_execute = PythonOperator(
+        task_id = "second_function_execute",
+        python_callable = second_function_execute,
+        provide_context = True)
+
+first_function_execute>>second_function_execute
 
 ## docker container ls - shows all running container
 ##  docker image prune - deletes all unnnecessary images
